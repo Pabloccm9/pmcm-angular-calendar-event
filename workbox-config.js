8ab8799688d82d6960c9111af7c3a2b68e636849
Pablo Manuel <pablomanuel.caceres@viewnext.com>
2023-06-22 09:37:52 +0200

    Revert types deletion

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'build/',
+	globPatterns: [
+		'**/*.{json,ico,html,png,txt,js,css}'
+	],
+	swDest: 'build/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
